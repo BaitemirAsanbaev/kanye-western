ee8bac8c134068a2e5f279324e3e34cb20e6332b
Baitemir <ferrari170604@gmail.com>
2021-03-10 13:48:03 +0600

    setted magazine

--- index.js
+++ index.js
@@ -14,18 +14,24 @@ document.body.addEventListener('mousemove', function(event){
 function reload(){
     bul.innerHTML = "bullets: " + bullets + '/30';
 }
-
+let magaz = 3;
+let mag = document.querySelector('#magaz');
 let interval;
 let bullets = 30;
 let bul = document.querySelector('#bullets')
 document.body.addEventListener('keydown', (event) =>{
-    if(event.keyCode == 82 && bullets > 25){
-        bullets = 30;
-        reload();
-    }else{
-        bullets +=5;
-        reload();
+    if( magaz > 0){
+        magaz --;
+        mag.innerHTML = 'magazine: ' + magaz;
+        if(event.keyCode == 82 && bullets > 25){
+            bullets = 30;
+            reload();
+        }else{
+            bullets +=5;
+            reload();
+        }
     }
+    
 })
 document.body.addEventListener('mousedown', function(event){
 
@@ -80,4 +86,24 @@ let int = setInterval(() => {
         scr.innerHTML = "score: " + score;
     })
 }, intervalTime);
+
+let intMag = setInterval(() => {
+    let plusMag = document.createElement('div');
+    document.body.append(plusMag);
+    plusMag.style.height = '100px';
+    plusMag.style.width = '100px';
+    plusMag.style.backgroundColor = 'red'
+    plusMag.style.backgroundSize = 'contain'
+    plusMag.style.position = 'absolute';
+    plusMag.style.top = (Math.floor(Math.random() * (700 - 100)) + 100) + 'px';
+    plusMag.style.left = (Math.floor(Math.random() * (1200 - 100)) + 100) + 'px';
+    
+    plusMag.addEventListener('mousedown', () => {
+        if(bullets > 0){
+            plusMag.style.display = 'none';
+            magaz ++;
+        }
+        mag.innerHTML = "Magazine: " + mag;
+    })
+}, 6000);
 
